ada833c8549edf73808c0797dcc469a4bebdd74a
musicizumi3 <musicizumi3@gmail.com>
2024-10-13 05:27:18 +0000

    init idx

---
+++ lib/webviewpage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'dart:io';
+
+class WebviewPage extends StatefulWidget {
+  const WebviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<WebviewPage> createState() => _WebviewPageState();
+}
+
+class _WebviewPageState extends State<WebviewPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://kordinator.nusaina.cloud',
+    );
+  }
+}
